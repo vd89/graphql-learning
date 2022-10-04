b45a1b4d406d6b263a3640b5b0ff390d23864934
vd89 <dixitvora@gmail.com>
2022-10-04 18:19:16 +0530

    main: added the field level resolver

---
+++ src/constants/index.js
@@ -0,0 +1,21 @@
+export const tasks = () => {
+  return [
+    { _id: 1, name: 'Work', completed: false, userId: '3' },
+    { _id: 2, name: 'Reading', completed: true, userId: '2' },
+    { _id: 3, name: 'Eat', completed: false, userId: '1' },
+    { _id: 4, name: 'Shopping', completed: true, userId: '4' },
+    { _id: 5, name: 'Gym', completed: true, userId: '2' },
+  ];
+};
+
+
+export const users = () => {
+  return [
+    { _id: '1', name: 'james', email: 'james@smith.com' },
+    { _id: '2', name: 'billy', email: 'billy@smith.com' },
+    { _id: '3', name: 'bob', email: 'bob@smith.com' },
+    { _id: '4', name: 'sam', email: 'sam@smith.com' },
+    { _id: '5', name: 'peter', email: 'peter@smith.com' },
+  ];
+};
+
--- src/gql/index.js
+++ src/gql/index.js
@@ -1,21 +1,42 @@
-/* eslint-disable new-cap */
 import _apolloServer from 'apollo-server-express';
 import {
   ApolloServerPluginLandingPageGraphQLPlayground,
 } from 'apollo-server-core';
+import { tasks, users } from '../constants/index.js';
 
 
 const { ApolloServer, gql } = _apolloServer;
 
 const typeDefs = gql`
   type Query {
-    hello: String
+    hello: String!
+    james: [Int!]!
+    tasks:[Task!]
+    users:[User!]
+  }
+  type User {
+    _id: ID!
+    name: String!
+    email: String!
+    tasks: [Task!]
+  }
+  type Task {
+    _id: ID!
+    name: String!
+    completed: Boolean!
+    user: User!
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    hello: () => 'Thi is working',
+    james: () => [1, 2, 3, 3],
+    tasks: () => tasks(),
+    users: () => users(),
+  },
+  Task: {
+    user: ({ userId }) => users().find((user) => user._id === userId),
   },
 };
 
@@ -24,7 +45,7 @@ const APServer = new ApolloServer({
   typeDefs,
   resolvers,
   plugins: [
-    ApolloServerPluginLandingPageGraphQLPlayground(),
+    ApolloServerPluginLandingPageGraphQLPlayground,
   ],
 });
 export default APServer;
