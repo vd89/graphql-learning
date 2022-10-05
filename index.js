6c049e1900dd454048f4b823e801f3578533a4f1
vd89 <dixitvora@gmail.com>
2022-10-05 14:45:31 +0530

    main: added the mongodb connection

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import debug from 'debug';
 import app from './src/app.js';
 import appConfig from './src/appConfig.js';
 import APServer from './src/gql/index.js';
+import dbConnection from './src/utils/dbConnection.js';
 
 const indexDebug = debug('app:index ->');
 const { port } = appConfig;
@@ -9,6 +10,7 @@ const { port } = appConfig;
 
 (async ()=>{
   indexDebug('Starting the server');
+  await dbConnection();
   app.listen(port, () => {
     try {
       indexDebug(`Server is running on the http://localhost:${port}`);
---
+++ src/utils/dbConnection.js
@@ -0,0 +1,23 @@
+import { connect } from 'mongoose';
+import debug from 'debug';
+import appConfig from '../appConfig.js';
+
+const logger = debug('app:dbConnection ->');
+const { mongoUrl } = appConfig;
+const mongoOpt = {
+  keepAlive: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 50000,
+  socketTimeoutMS: 50000,
+};
+
+const dbConnection = async () => {
+  try {
+    await connect(mongoUrl, mongoOpt);
+    logger(`MongoDb -> Connected to mongoDb Server`);
+  } catch (err) {
+    logger(err.message);
+  }
+};
+export default dbConnection;
